cca56175e7b6f56650516a5070b7115f566fdb00
Norbert Schmidt <github@ddq.nl>
2025-06-10 09:53:45 +0200

    Update core.php

--- core.php
+++ core.php
@@ -1,6 +1,8 @@
 <?php
 
 session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // Check if user is logged in
 if (!isset($_SESSION['DienstID']) || !isset($_SESSION['Systeem'])) {
